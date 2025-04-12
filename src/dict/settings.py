7bc12d6c70a45a02c73768a5cb2aa270a525d086
rinmz <reisinanmaz@gmail.com>
2025-04-12 13:42:46 -0500

    (feat) added staticfiles and whitenoise

--- src/dict/settings.py
+++ src/dict/settings.py
@@ -57,8 +57,12 @@ INSTALLED_APPS = [
     'commando',
 ]
 
+if DEBUG:
+    INSTALLED_APPS.append('whitenoise.runserver_nostatic')
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,6 +147,26 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# send static files here
+# locked files that do not change during runtime
+# external static  file server
+STATIC_ROOT = BASE_DIR / 'static_root'
+STATIC_ROOT.mkdir(exist_ok=True, parents=True)
+
+# retain a copy of the static files here
+# like custom css
+# unlocked files that change during runtime
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles',
+]
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
